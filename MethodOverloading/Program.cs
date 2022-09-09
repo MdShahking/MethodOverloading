7c6e0a7733374bf79075b02a696efb17c8c7071f
Shahnawaz <shahnawazhussain738@gmail.com>
2022-09-09 18:06:05 +0530

    type of polymorphism

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Program
+    {
+        void sum (int a,int b)
+        {
+            Console.WriteLine( a + b);
+        }
+        void sum (float a, float b)
+        {
+            Console.WriteLine ( a + b);
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.sum(10.2f, 20f);
+            Console.ReadLine();
+        }
+    }
+}
